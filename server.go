b231b4a7918c59cc72cff3f377e1c576a978377e
Taylor-J-Smith <taylor.smith1@uoit.net>
2016-04-20 17:08:34 -0400

    Implement templating, view/edit rendering and regexp checking path name. This should have been many commits

--- server.go
+++ server.go
@@ -4,6 +4,9 @@ import (
     "fmt"
     "io/ioutil"
     "net/http"
+    "html/template"
+    "regexp"
+    "errors"
 )
 
 // Basic Page struct.
@@ -23,8 +26,9 @@ func (p *Page) save() error {
 // Loads a page from memory with the title given in the parameter.
 // Returns a nil string if the page does not exist otherwise returns a page.
 func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := ioutil.ReadFile(filename)
+	filename := "pages/" + title + ".txt"
+
+  body, err := ioutil.ReadFile(filename)
 
   if err != nil {
 		return nil, err
@@ -33,17 +37,73 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+var templates = template.Must(template.ParseFiles("tmpl/edit.html", "tmpl/view.html"))
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+    err := templates.ExecuteTemplate(w, tmpl+".html", p)
+
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
+
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+
+    m := validPath.FindStringSubmatch(r.URL.Path)
+
+    if m == nil {
+        http.NotFound(w, r)
+        return "", errors.New("Invalid Page Title")
+    }
+
+    return m[2], nil // The title is the second subexpression.
+}
+
 // Basic handler used for demo purposes.
 func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
-func main() {
-    p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-    p1.save()
-    p2, _ := loadPage("TestPage")
-    fmt.Println(string(p2.Body))
+// Redirects the root call to the front page of the wiki.
+func rootRedirectHandler(w http.ResponseWriter, r *http.Request) {
+  http.Redirect(w, r, "/view/FrontPage", http.StatusFound)
+}
 
-    http.HandleFunc("/", handler)
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+
+    title, err := getTitle(w, r)
+
+    if err != nil {
+        return
+    }
+
+    p, err := loadPage(title)
+
+    if err != nil {
+        http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+        return
+    }
+    renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+    title, err := getTitle(w, r)
+
+    p, err := loadPage(title)
+
+    if err != nil {
+        p = &Page{Title: title}
+    }
+
+    renderTemplate(w, "edit", p)
+}
+
+func main() {
+
+    http.HandleFunc("/", rootRedirectHandler)
+    http.HandleFunc("/view/", viewHandler)
+    http.HandleFunc("/edit/", editHandler)
     http.ListenAndServe(":8080", nil)
 }
